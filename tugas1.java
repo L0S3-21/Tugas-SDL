62308ff4d2bb59c5659406ef21a669558ff1ff27
Novarian-any <nvrdst26@gmail.com>
2026-02-20 03:49:46 +0700

    Update tugas1 - tambah total dan rata-rata

--- tugas1.java
+++ tugas1.java
@@ -1,11 +1,17 @@
 public class tugas1 {
     public static void main(String[] args) {
-        int[] nilai = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int[] nilai = {1,2,3,4,5,6,7,8,9,10};
+
         double total = 0;
 
         for (int i = 0; i < nilai.length; i++) {
             total += nilai[i];
         }
-        System.out.println(total);
+
+        double rataRata = total / nilai.length;
+
+        System.out.println("Total Penjumlahan : " + total);
+        System.out.println("RATA-RATA: " + rataRata);
     }
 }
